3a63af0ce6b310b671b14e348862eaa4054b7907
gugus2000 <gugus2000@protonmail.com>
2022-03-21 19:56:18 +0100

    Finish \route\Router translation

---
+++ src/lang/class/route/Router/fr.lang.php
@@ -0,0 +1,94 @@
+<?php
+
+/** [buildNode] **/
+/* start the process */
+$GLOBALS['lang']['class']['route']['Router']['buildNode']['start'] = 'début de la construction d\'un noeud pour vérifier le chemin de la route';
+/* array is too empty */
+$GLOBALS['lang']['class']['route']['Router']['buildNode']['undefined'] = 'indice hors de portée, la liste n\'a pas {index} colonnes';
+/* node empty after the process */
+$GLOBALS['lang']['class']['route']['Router']['buildNode']['empty_node'] = 'noeud vide après le processus';
+/* end of the process */
+$GLOBALS['lang']['class']['route']['Router']['buildNode']['end'] = 'noeud construit';
+/** [/buildNode] **/
+
+/** [cleanParameters] **/
+/* start the process */
+$GLOBALS['lang']['class']['route']['Router']['cleanParameters']['start'] = 'début du nettoyage des paramètres de {page}';
+/* parameter found */
+$GLOBALS['lang']['class']['route']['Router']['cleanParameters']['found'] = 'le paramètre {name} avec la valeur {value} correspondant à {regex} a été trouvé dans la page {page}';
+/* missing parameter */
+$GLOBALS['lang']['class']['route']['Router']['cleanParameters']['missing'] = 'le paramètre {name} avec la valeur {value} correspondant à {regex} n\'a pas été trouvé dans la page {page} et est pourtant nécessaire';
+/* end of the process */
+$GLOBALS['lang']['class']['route']['Router']['cleanParameters']['end'] = 'parameters cleaned';
+/** [/cleanParameters] **/
+
+/** [createLink] **/
+/* empty array */
+$GLOBALS['lang']['class']['route']['Router']['createLink']['empty'] = 'impossible de créer un noeud sans chemin de la route vers la page';
+/* unknown Router mode */
+$GLOBALS['lang']['class']['route']['Router']['createLink']['unknown_mode'] = 'le mode de routeur {mode} n\'existe pas';
+/** [/createLink] **/
+
+/** [createLinkGet] **/
+/* empty array */
+$GLOBALS['lang']['class']['route']['Router']['createLinkGet']['empty'] = 'impossible de créer un lien avec le mode GET sans aucun chemins de la route vers la page';
+/* path not found */
+$GLOBALS['lang']['class']['route']['Router']['createLinkGet']['not_found'] = 'impossible d\'obtenir le chemin de la route {route}';
+/* path is file */
+$GLOBALS['lang']['class']['route']['Router']['createLinkGet']['file'] = '{file} est un fichier';
+/* link created */
+$GLOBALS['lang']['class']['route']['Router']['createLinkGet']['success'] = 'lien créé : {link}';
+/** [/createLinkGet] **/
+
+/** [createLinkMixed] **/
+/* empty array */
+$GLOBALS['lang']['class']['route']['Router']['createLinkMixed']['empty'] = 'impossible de créer un lien avec le mode MIXED sans aucun chemins de la route vers la page';
+/* path not found */
+$GLOBALS['lang']['class']['route']['Router']['createLinkMixed']['not_found'] = 'impossible d\'obtenir le chemin de la route {route}';
+/* path is file */
+$GLOBALS['lang']['class']['route']['Router']['createLinkMixed']['file'] = '{file} est un fichier';
+/* link created */
+$GLOBALS['lang']['class']['route']['Router']['createLinkMixed']['success'] = 'lien créé : {link}';
+/** [/createLinkMixed] **/
+
+/** [createLinkRoute] **/
+/* empty array */
+$GLOBALS['lang']['class']['route']['Router']['createLinkRoute']['empty'] = 'impossible de créer un lien avec le mode ROUTE sans aucun chemins de la route vers la page';
+/* path not found */
+$GLOBALS['lang']['class']['route']['Router']['createLinkRoute']['not_found'] = 'impossible d\'obtenir le chemin de la route {route}';
+/* path is file */
+$GLOBALS['lang']['class']['route']['Router']['createLinkRoute']['file'] = '{file} est un fichier';
+/* link created */
+$GLOBALS['lang']['class']['route']['Router']['createLinkRoute']['success'] = 'lien créé : {link}';
+/** [/createLinkRoute] **/
+
+/** [decodeRoute] **/
+/* empty url */
+$GLOBALS['lang']['class']['route']['Router']['decodeRoute']['empty'] = 'impossible de décoder une url vide';
+/* unknown Router mode */
+$GLOBALS['lang']['class']['route']['Router']['decodeRoute']['unknown_mode'] = 'le mode de routeur {mode} n\'existe pas';
+/** [/decodeRoute] **/
+
+/** [decodeWithGet] **/
+/* path not defined */
+$GLOBALS['lang']['class']['route']['Router']['decodeWithGet']['no_path'] = 'pas de chemin trouvé pour cet url';
+/** [/decodeWithGet] **/
+
+/** [decodeWithMixed] **/
+/* unknown route */
+$GLOBALS['lang']['class']['route']['Router']['decodeWithMixed']['unknown_route'] = 'route inconnue pour {url}';
+/** [/decodeWithMixed] **/
+
+/** [decodeWithRoute] **/
+/* start to decode parameter list */
+$GLOBALS['lang']['class']['route']['Router']['decodeWithRoute']['start_parameter_list'] = 'début du décodage de la liste des paramètres';
+/* unknown route */
+$GLOBALS['lang']['class']['route']['Router']['decodeWithRoute']['unknown_route'] = 'route incconue pour {url}';
+/** [/decodeWithRoute] **/
+
+/** [setMode] **/
+/* unknown mode */
+$GLOBALS['lang']['class']['route']['Router']['setMode']['unknown'] = 'mode {mode} inconnu';
+/** [/setMode] **/
+
+?>
