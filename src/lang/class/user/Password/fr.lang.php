cee97590d1b3bea61da87fcd675303ed79f82d1a
gugus2000 <gugus2000@protonmail.com>
2022-03-21 20:29:17 +0100

    Add \user\Password translation

---
+++ src/lang/class/user/Password/fr.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/** [check] **/
+/* hashed password is defined */
+$GLOBALS['lang']['class']['user']['Password']['check']['loading_hashed'] = 'mot de passe hashé défini';
+/* cannot retrieve hashed password */
+$GLOBALS['lang']['class']['user']['Password']['check']['cannot_retrieve_hashed'] = 'il n\'y a pas de mot de passe avec ce hash';
+/* hashed password is not defined */
+$GLOBALS['lang']['class']['user']['Password']['check']['no_clear'] = 'pas de mot de passe en clair, abandon';
+/* clear password is verified */
+$GLOBALS['lang']['class']['user']['Password']['check']['verified'] = 'le mot de passe en clair et le mot de passe hashé correspondent';
+/* password needs to be rehashed */
+$GLOBALS['lang']['class']['user']['Password']['check']['need_rehash'] = 'le mot de passe hashé a besoin d\'être re-ashé';
+/* password missmatch */
+$GLOBALS['lang']['class']['user']['Password']['check']['bad_credential'] = 'le mot de passe et le nom d\'utilisateur ne correspondent pas';
+/** [/check] **/
+
+?>
