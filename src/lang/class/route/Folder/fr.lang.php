2bcbd083041082ad99e55f37fd0c8980107540bf
gugus2000 <gugus2000@protonmail.com>
2022-03-21 02:03:07 +0100

    Add \route\Folder translation

---
+++ src/lang/class/route/Folder/fr.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/** [getParent] **/
+/* id_parent is null for this folder */
+$GLOBALS['lang']['class']['route']['Folder']['getParent']['null'] = 'ce dossier n\' a pas de parent';
+/** [/getParent] **/
+
+/** [getPath] **/
+/* id_parent is null for this folder, no path to return */
+$GLOBALS['lang']['class']['route']['Folder']['getPath']['null'] = 'impossible d\'obtenir le chemin d\'un dossier sans aucun parent (même pas la racine)';
+/** [/getPath] **/
+
+?>
