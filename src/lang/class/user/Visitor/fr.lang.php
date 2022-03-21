13826bd6fadabd787d02e933eb3d83c2b42ca46a
gugus2000 <gugus2000@protonmail.com>
2022-03-21 20:48:14 +0100

    Add \user\Visitor tranlsation

---
+++ src/lang/class/user/Visitor/fr.lang.php
@@ -0,0 +1,39 @@
+<?php
+
+/** [connect] **/
+/* id not defined */
+$GLOBALS['lang']['class']['user']['Visitor']['connect']['no_id'] = 'impossible de connecter le visiteur, son id n\'est pas défini';
+/* password not defined */
+$GLOBALS['lang']['class']['user']['Visitor']['connect']['no_password'] = 'impossible de connecter le visiteur, son mot de passe n\'eset pas défini';
+/* user does not exist */
+$GLOBALS['lang']['class']['user']['Visitor']['connect']['invalid_id'] = 'impossible de connecter le visiteur, l\'utilisateur n\'existe pas';
+/* password missmatch */
+$GLOBALS['lang']['class']['user']['Visitor']['connect']['bad_credential'] = 'impossible de connecter le visiteur, l\'id et le le mot de passe ne correspondent pas';
+/* success, the user is connected */
+$GLOBALS['lang']['class']['user']['Visitor']['connect']['success'] = 'le visiteur s\'est bien connecté';
+/** [/connect] **/
+
+/** [disconnect] **/
+/* disconnect the user */
+$GLOBALS['lang']['class']['user']['Visitor']['disconnect'] = 'visiteur déconnecté';
+/** [/disconnect] **/
+
+/** [loadPage] **/
+/* load the Page */
+$GLOBALS['lang']['class']['user']['Visitor']['loadPage']['start'] = 'début du chargement de la page';
+/* visitor does not have the permission to load this page */
+$GLOBALS['lang']['class']['user']['Visitor']['loadPage']['no_permission'] = 'le visiteur n\'a pas la permission d\'accéder à cette page';
+/** [/loadPage] **/
+
+/** [register] **/
+/* start to register the visitor */
+$GLOBALS['lang']['class']['user']['Visitor']['register']['start'] = 'début de la création du visiteur';
+/* visitor does not have a defined password attribute */
+$GLOBALS['lang']['class']['user']['Visitor']['register']['no_password'] = 'le visiteur n\'a pas d\'attribut password défini';
+/* visitor does not have a defined roles attribute */
+$GLOBALS['lang']['class']['user']['Visitor']['register']['no_roles'] = 'le visiteur n\'a pas d\'attribut role défini';
+/* visitor registered */
+$GLOBALS['lang']['class']['user']['Visitor']['register']['end'] = 'le visiteur est enregistré';
+/** [/register] **/
+
+?>
