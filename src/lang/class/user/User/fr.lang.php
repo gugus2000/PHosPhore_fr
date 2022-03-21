00f3c5a66e09d351abf817b330d92d77faeda119
gugus2000 <gugus2000@protonmail.com>
2022-03-21 20:41:59 +0100

    Add \user\User translation

---
+++ src/lang/class/user/User/fr.lang.php
@@ -0,0 +1,58 @@
+<?php
+
+/** [checkPermission] **/
+/* start of the process */
+$GLOBALS['lang']['class']['user']['User']['checkPermission']['start'] = 'début de la vérification de la permission de l\'utilisateur {user} à la page/route {page}';
+/* permission found */
+$GLOBALS['lang']['class']['user']['User']['checkPermission']['found'] = 'permission trouvé pour l\'utilisateur {user} pour la page/route {page}';
+/* permission not found */
+$GLOBALS['lang']['class']['user']['User']['checkPermission']['not_found'] = 'permission non trouvé pour l\'utilisateur {user} pour la page/route {page}';
+/** [/checkPermission] **/
+
+/** [remove] **/
+/* start of the process */
+$GLOBALS['lang']['class']['user']['User']['remove']['start'] = 'début de la suppression de l\'utilisateur {user}';
+/* user deleted */
+$GLOBALS['lang']['class']['user']['User']['remove']['deleting_user'] = 'l\'utilisateur {user} a été supprimé';
+/** [/remove] **/
+
+/** [isConnected] **/
+/* checking if an user is connected */
+$GLOBALS['lang']['class']['user']['User']['isConnected'] = 'vérification de la connexion d\'un utilisateur';
+/** [/isConnected] **/
+
+/** [retrieve] **/
+/* retrieve the user */
+$GLOBALS['lang']['class']['user']['User']['retrieve'] = 'récupération des données d\'un utilisateur';
+/** [/retrieve] **/
+
+/** [retrieveConfigurations] **/
+/* retrieve an user configuration */
+$GLOBALS['lang']['class']['user']['User']['retrieveConfigurations'] = 'récupération de la configuration d\'un utilisateur';
+/** [/retrieveConfigurations] **/
+
+/** [retrieveNotifications] **/
+/* retrieve an user notification */
+$GLOBALS['lang']['class']['user']['User']['retrieveNotifications'] = 'récupération des notifications d\'un utilisateur';
+/** [/retrieveNotifications] **/
+
+/** [retrievePassword] **/
+/* retrieve an user password */
+$GLOBALS['lang']['class']['user']['User']['retrievePassword'] = 'récupération du mot de passe d\'un utilisateur';
+/** [/retrievePassword] **/
+
+/** [retrieveRoles] **/
+/* retrieve an user roles */
+$GLOBALS['lang']['class']['user']['User']['retrieveRoles'] = 'récupération des rôles d\'un utilisateur';
+/** [/retrieveRoles] **/
+
+/** [setConfiguration] **/
+/* start of the process */
+$GLOBALS['lang']['class']['user']['User']['setConfiguration']['start'] = 'début de l\'ajout ou de la mise à jour d\'une des configurations de l\'utilisateur';
+/* end of the process by updating an existing configuration */
+$GLOBALS['lang']['class']['user']['User']['setConfiguration']['end_replace'] = 'configuration mise à jour';
+/* end of the process by adding a new configuration */
+$GLOBALS['lang']['class']['user']['User']['setConfiguration']['end_add'] = 'configuration ajoutée';
+/** [/setConfiguration] **/
+
+?>
