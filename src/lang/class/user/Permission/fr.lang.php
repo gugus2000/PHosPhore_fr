85ad4d019cd96e9aa50b33c10a36fb6168a956f9
gugus2000 <gugus2000@protonmail.com>
2022-03-21 20:30:28 +0100

    Add \user\Permission translation

---
+++ src/lang/class/user/Permission/fr.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/** [retrieveRoute] **/
+/* start of the retrieval */
+$GLOBALS['lang']['class']['user']['Permission']['retrieveRoute'] = 'début de la récupération des permissions associées à la route';
+/** [/retrieveRoute] **/
+
+?>
