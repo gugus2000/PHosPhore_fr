eb16712ffde056ba7a19c9b93d0c0f7fbd955d6e
gugus2000 <gugus2000@protonmail.com>
2022-03-21 02:00:46 +0100

    Add \core\Token translation

---
+++ src/lang/class/core/Token/fr.lang.php
@@ -0,0 +1,25 @@
+<?php
+
+/** [check] **/
+/* start of the process */
+$GLOBALS['lang']['class']['core']['Token']['check']['start'] = 'début de la vérification du token';
+/* no selector attribute */
+$GLOBALS['lang']['class']['core']['Token']['check']['no_selector'] = 'impossible de vérifier le token sans la valeur du sélecteur';
+/* no validator_clear attribute */
+$GLOBALS['lang']['class']['core']['Token']['check']['no_validator_clear'] = 'impossible de vérifier le token sans la validateur (en clair)';
+/* no data corresponding to a valid token */
+$GLOBALS['lang']['class']['core']['Token']['check']['no_result'] = 'le token n\'a jamais existé, ou n\'est plus valide';
+/* incorrect token */
+$GLOBALS['lang']['class']['core']['Token']['check']['incorrect'] = 'le token n\'est pas valide';
+/* correct token */
+$GLOBALS['lang']['class']['core']['Token']['check']['correct'] = 'le token est valide';
+/** [/check] **/
+
+/** [create] **/
+/* start of the process */
+$GLOBALS['lang']['class']['core']['Token']['create']['start'] = 'début de la création d\'un nouveau token';
+/* attribute id undefined */
+$GLOBALS['lang']['class']['core']['Token']['create']['no_id'] = 'impossible de créer un nouveau token sans id';
+/** [/create] **/
+
+?>
