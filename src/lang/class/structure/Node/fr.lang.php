be766d602a187a95dc585897b406aeef0541d6ae
gugus2000 <gugus2000@protonmail.com>
2022-03-21 19:59:29 +0100

    Add \Structure translation

---
+++ src/lang/class/structure/Node/fr.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/** [get] **/
+/* attribute not defined */
+$GLOBALS['lang']['class']['structure']['Node']['get']['not_found'] = 'attribut {attribute} non trouvé';
+/** [/get] **/
+
+/** [set] **/
+/* attribute not exist */
+$GLOBALS['lang']['class']['structure']['Node']['set']['not_found'] = 'attribut {attribute} non défini';
+/** [/set] **/
+
+?>
---
+++ src/lang/class/structure/Tree/fr.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/** [get] **/
+/* attribute not found */
+$GLOBALS['lang']['class']['structure']['Tree']['get']['not_found'] = 'attrubut {attribute} non trouvé';
+/** [/get] **/
+
+/** [set] **/
+/* attribute not found */
+$GLOBALS['lang']['class']['structure']['Tree']['set']['not_found'] = 'attribute {attribute} non défini';
+/** [/set] **/
+
+?>
