ec8e76b944b467e84dd7d639383cbcd6a215811b
gugus2000 <gugus2000@protonmail.com>
2022-03-21 20:32:04 +0100

    Add \user\Role translation

---
+++ src/lang/class/user/Role/fr.lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/** [addPermissions] **/
+/* Add the permissions */
+$GLOBALS['lang']['class']['user']['Role']['add']['start'] = 'début de la création d\'un rôle utilisateur: association {role}--{user}';
+/** [/addPermissions] **/
+
+/** [retrievePermissions] **/
+/* Retrieve the Permissions of this Role */
+$GLOBALS['lang']['class']['user']['Role']['getPermissions'] = 'récupération des permissions de ce rôle';
+/** [/retrievePermissions] **/
+
+?>
